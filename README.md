deb3c7d5b3e6022769db6e439e328da1a594fafc
Ramesh Iyyar <rameshi1@in.ibm.com>
2021-06-25 05:35:46 -0500

    op-hw-isolation: Added a new D-Bus daemon application
    - The added D-Bus daemon application is a skeleton to enable hardware isolation
      feature (isolate faulty hardware from the boot of the host.)
      Please refer to the below link to get more about hardware isolation.
      https://github.com/openbmc/docs/blob/master/designs/guard-on-bmc.md
    
    - The added skeleton application will host the below standard
      D-Bus interfaces.
    
    ```
    NAME                                TYPE      SIGNATURE  RESULT/VALUE  FLAGS
    org.freedesktop.DBus.Introspectable interface -          -             -
    .Introspect                         method    -          s             -
    org.freedesktop.DBus.ObjectManager  interface -          -             -
    .GetManagedObjects                  method    -          a{oa{sa{sv}}} -
    .InterfacesAdded                    signal    oa{sa{sv}} -             -
    .InterfacesRemoved                  signal    oas        -             -
    org.freedesktop.DBus.Peer           interface -          -             -
    .GetMachineId                       method    -          s             -
    .Ping                               method    -          -             -
    org.freedesktop.DBus.Properties     interface -          -             -
    .Get                                method    ss         v             -
    .GetAll                             method    s          a{sv}         -
    .Set                                method    ssv        -             -
    .PropertiesChanged                  signal    sa{sv}as   -             -
    ```
    
    Tested:
    - Tested by using `busctl tree` to make sure the skeleton application hosting
      the configured D-Bus root path for hardware isolation application.
    
    ```
    busctl tree | grep hardware_isolation
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/hardware_isolation
    ```
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>
    Change-Id: Ie15b8f464104fbd045c6a1b4debcc6c115a4ed25

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ isolated hardware will be ignored to init during the next boot of the host.
 **Note:**
 - The system must be in a power-off state when a user wants to isolate or deisolate hardware.
 - The isolated hardware details will be considered only in the next boot of the host to isolate.
+
+### To build
+```
+meson builddir
+ninja -C builddir
+```
---
+++ hardware_isolation_manager_main.cpp
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#include "config.h"
+
+#include <fmt/format.h>
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <sdeventplus/event.hpp>
+
+int main()
+{
+    auto eventLoopRet = 0;
+    try
+    {
+        auto bus = sdbusplus::bus::new_default();
+        bus.request_name(HW_ISOLATION_BUSNAME);
+
+        auto event = sdeventplus::Event::get_default();
+        bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+
+        // Add sdbusplus ObjectManager for the 'root' path of the hardware
+        // isolation manager.
+        sdbusplus::server::manager::manager objManager(bus,
+                                                       HW_ISOLATION_OBJPATH);
+
+        // The below statement should be last to enter this app into the loop
+        // to process D-Bus services.
+        eventLoopRet = event.loop();
+    }
+    catch (std::exception& e)
+    {
+        using namespace phosphor::logging;
+        log<level::ERR>(fmt::format("Exception [{}]", e.what()).c_str());
+    }
+
+    return eventLoopRet;
+}
