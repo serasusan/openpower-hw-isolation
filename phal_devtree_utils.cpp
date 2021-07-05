ba387cde2d6c837b325d6cfe89de2cbd3ba6e3de
Ramesh Iyyar <rameshi1@in.ibm.com>
2021-07-04 23:39:45 -0500

    phal_devtree: Added lookup functions to get physical path
    - In an OpenPOWER based system, the hardware isolation using a
      defined record format which will expect hardware EntityPath
      and it present in phal cec device tree.
    
    - In OpenBMC, the few cec hardware (processor, memory, core, etc)
      are modeled in BMC inventory and all instance lookup will be based on
      inventory object path for example (cpu0) but, in phal cec device tree
      it needs to decide based on some attributes (which will vary based on
      hardware and system package) so added phal cec device tree lookup
      functions to get EntityPath of hardware to isolate into an OpenPOWER
      based system.
    
    - The lookup functions added for below attributes to get physical path
      - ATTR_MRU_ID - used for processor.
      - ATTR_LOCATION_CODE - used for memory (aka dimm).
      - ATTR_CHIP_UNIT_POS - used for core.
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>
    Change-Id: Id7393a1c9122a405430438d955e63bc63d9f10a6

--- phal_devtree_utils.cpp
+++ phal_devtree_utils.cpp
@@ -7,6 +7,8 @@ extern "C"
 
 #include "config.h"
 
+#include "attributes_info.H"
+
 #include "phal_devtree_utils.hpp"
 
 #include <fmt/format.h>
@@ -50,6 +52,85 @@ void initPHAL()
         throw std::runtime_error("pdbg target initialization failed");
     }
 }
+
+namespace lookup_func
+{
+CanGetPhysPath mruId(struct pdbg_target* pdbgTgt, InstanceId instanceId,
+                     LocationCode locCode)
+{
+    CanGetPhysPath canGetPhysPath = false;
+
+    ATTR_MRU_ID_Type devTreeMruId;
+    if (DT_GET_PROP(ATTR_MRU_ID, pdbgTgt, devTreeMruId))
+    {
+        throw std::runtime_error(
+            std::string("Failed to get ATTR_MRU_ID from ") +
+            pdbg_target_path(pdbgTgt));
+    }
+
+    // Last two byte (from MSB) of MRU_ID having instance number
+    if ((devTreeMruId & 0xFFFF) == instanceId)
+    {
+        canGetPhysPath = true;
+    }
+
+    // If given target having location attribute then check that with given
+    // location code.
+    ATTR_LOCATION_CODE_Type devTreelocCode;
+    if (!DT_GET_PROP(ATTR_LOCATION_CODE, pdbgTgt, devTreelocCode) &&
+        (canGetPhysPath == true))
+    {
+        // If location code did not match then given device tree target is not
+        // expected one.
+        if (LocationCode(devTreelocCode) != locCode)
+        {
+            canGetPhysPath = false;
+        }
+    }
+
+    return canGetPhysPath;
+}
+
+CanGetPhysPath chipUnitPos(struct pdbg_target* pdbgTgt, InstanceId instanceId,
+                           LocationCode /* locCode */)
+{
+    CanGetPhysPath canGetPhysPath = false;
+
+    ATTR_CHIP_UNIT_POS_Type devTreeChipUnitPos;
+    if (DT_GET_PROP(ATTR_CHIP_UNIT_POS, pdbgTgt, devTreeChipUnitPos))
+    {
+        throw std::runtime_error(
+            std::string("Failed to get ATTR_CHIP_UNIT_POS from ") +
+            pdbg_target_path(pdbgTgt));
+    }
+    if (devTreeChipUnitPos == instanceId)
+    {
+        canGetPhysPath = true;
+    }
+    return canGetPhysPath;
+}
+
+CanGetPhysPath locationCode(struct pdbg_target* pdbgTgt,
+                            InstanceId /* instanceId */, LocationCode locCode)
+{
+    CanGetPhysPath canGetPhysPath = false;
+
+    ATTR_LOCATION_CODE_Type devTreelocCode;
+    if (DT_GET_PROP(ATTR_LOCATION_CODE, pdbgTgt, devTreelocCode))
+    {
+        throw std::runtime_error(
+            std::string("Failed to get ATTR_LOCATION_CODE from ") +
+            pdbg_target_path(pdbgTgt));
+    }
+
+    if (LocationCode(devTreelocCode) == locCode)
+    {
+        canGetPhysPath = true;
+    }
+    return canGetPhysPath;
+}
+
+} // namespace lookup_func
 
 } // namespace devtree
 } // namespace hw_isolation
--- phal_devtree_utils.hpp
+++ phal_devtree_utils.hpp
@@ -2,10 +2,21 @@
 
 #pragma once
 
+extern "C"
+{
+#include <libpdbg.h>
+}
+
+#include <functional>
+#include <string>
+
 namespace hw_isolation
 {
 namespace devtree
 {
+
+using LocationCode = std::string;
+using InstanceId = uint32_t;
 
 /**
  * @brief API to init PHAL (POWER Hardware Abstraction Layer)
@@ -14,5 +25,44 @@ namespace devtree
  */
 void initPHAL();
 
+/**
+ * @brief Used to add functions that will use to get to know
+ *        whether the given target (aka device tree node) can
+ *        be used to get the physical path of expected hardware.
+ */
+namespace lookup_func
+{
+
+using CanGetPhysPath = bool;
+
+/**
+ * @brief Lookup function signature
+ *
+ * @param[in] pdbg_target - phal cec device tree target (node).
+ * @param[in] InstanceId - instance id of hardware to check with
+ *                         phal cec device tree.
+ * @param[in] LocationCode - location code of hardware to check
+ *                           with phal cec device tree.
+ *                           This is optional if hardware is non-fru.
+ *
+ * @return CanGetPhysPath to indicate whether can get the physical path
+ *         or not from given phal cec device tree target.
+ *
+ * @note All lookup functions which are added in this namespace should
+ *       match with below signature.
+ */
+using LookupFuncForPhysPath = std::function<CanGetPhysPath(
+    struct pdbg_target*, InstanceId, LocationCode)>;
+
+CanGetPhysPath mruId(struct pdbg_target* pdbgTgt, InstanceId instanceId,
+                     LocationCode locCode);
+
+CanGetPhysPath chipUnitPos(struct pdbg_target* pdbgTgt, InstanceId instanceId,
+                           LocationCode locCode);
+
+CanGetPhysPath locationCode(struct pdbg_target* pdbgTgt, InstanceId instanceId,
+                            LocationCode locCode);
+
+} // namespace lookup_func
 } // namespace  devtree
 } // namespace hw_isolation
