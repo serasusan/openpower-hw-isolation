2d4de3a9225eaec1422c3e1e314fde96310ecb88
Ramesh Iyyar <rameshi1@in.ibm.com>
2021-08-03 07:44:35 -0500

    op-guard: Added the common exception handler macro
    - The libguard interface will throw an exception and those need to
      as per OpenBMC defined exception.
    
    - The exception handler will be common for all libguard interfaces
      so added the common macro for maintainability and reusability.
    
    - Added the new exception "GuardFileSeekFailed" handler.
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>
    Change-Id: I94f20811c4ecf47bde1418d0832f6927c605b428

--- openpower_guard_interface.cpp
+++ openpower_guard_interface.cpp
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
-#include "common_types.hpp"
 #include "openpower_guard_interface.hpp"
+
+#include "common_types.hpp"
 
 #include <fmt/format.h>
 
@@ -20,141 +21,75 @@ namespace FileError = sdbusplus::xyz::openbmc_project::Common::File::Error;
 namespace HardwareIsolationError =
     sdbusplus::xyz::openbmc_project::HardwareIsolation::Error;
 
+/**
+ * @brief Helper MACRO to call libguard interface and to have
+ *        common exception handler
+ *
+ * @param[in] INTERFACE_SIGNATURE - To replace inside try block
+ */
+#define CALL_LIBGUARD_INTERFACE(INTERFACE_SIGNATURE)                           \
+    try                                                                        \
+    {                                                                          \
+        INTERFACE_SIGNATURE                                                    \
+    }                                                                          \
+    catch (libguard::exception::GuardFileOpenFailed & e)                       \
+    {                                                                          \
+        throw FileError::Open();                                               \
+    }                                                                          \
+    catch (libguard::exception::GuardFileReadFailed & e)                       \
+    {                                                                          \
+        throw FileError::Read();                                               \
+    }                                                                          \
+    catch (libguard::exception::GuardFileWriteFailed & e)                      \
+    {                                                                          \
+        throw FileError::Write();                                              \
+    }                                                                          \
+    catch (libguard::exception::GuardFileSeekFailed & e)                       \
+    {                                                                          \
+        throw FileError::Seek();                                               \
+    }                                                                          \
+    catch (libguard::exception::InvalidEntityPath & e)                         \
+    {                                                                          \
+        throw type::CommonError::InvalidArgument();                            \
+    }                                                                          \
+    catch (libguard::exception::AlreadyGuarded & e)                            \
+    {                                                                          \
+        throw HardwareIsolationError::IsolatedAlready();                       \
+    }                                                                          \
+    catch (libguard::exception::GuardFileOverFlowed & e)                       \
+    {                                                                          \
+        throw type::CommonError::NotAllowed();                                 \
+    }
+
 std::optional<GuardRecord> create(const EntityPath& entityPath,
                                   const uint32_t errorLogId,
                                   const GardType guardType)
 {
-    try
-    {
-        return libguard::create(entityPath, errorLogId, guardType);
-    }
-    catch (libguard::exception::GuardFileOpenFailed& e)
-    {
-        throw FileError::Open();
-    }
-    catch (libguard::exception::GuardFileReadFailed& e)
-    {
-        throw FileError::Read();
-    }
-    catch (libguard::exception::GuardFileWriteFailed& e)
-    {
-        throw FileError::Write();
-    }
-    catch (libguard::exception::InvalidEntityPath& e)
-    {
-        throw type::CommonError::InvalidArgument();
-    }
-    catch (libguard::exception::AlreadyGuarded& e)
-    {
-        throw HardwareIsolationError::IsolatedAlready();
-    }
-    catch (libguard::exception::GuardFileOverFlowed& e)
-    {
-        throw type::CommonError::NotAllowed();
-    }
+    CALL_LIBGUARD_INTERFACE(
+        return libguard::create(entityPath, errorLogId, guardType);)
+
     return std::nullopt;
 }
 
 void clear(const uint32_t recordId)
 {
-    try
-    {
-        libguard::clear(recordId);
-    }
-    catch (libguard::exception::GuardFileOpenFailed& e)
-    {
-        throw FileError::Open();
-    }
-    catch (libguard::exception::GuardFileReadFailed& e)
-    {
-        throw FileError::Read();
-    }
-    catch (libguard::exception::GuardFileWriteFailed& e)
-    {
-        throw FileError::Write();
-    }
-    catch (libguard::exception::InvalidEntityPath& e)
-    {
-        throw type::CommonError::InvalidArgument();
-    }
-    catch (libguard::exception::AlreadyGuarded& e)
-    {
-        throw HardwareIsolationError::IsolatedAlready();
-    }
-    catch (libguard::exception::GuardFileOverFlowed& e)
-    {
-        throw type::CommonError::NotAllowed();
-    }
+    CALL_LIBGUARD_INTERFACE(libguard::clear(recordId);)
 }
 
 GuardRecords getAll()
 {
     GuardRecords records;
-    try
-    {
-        records = libguard::getAll();
-    }
-    catch (libguard::exception::GuardFileOpenFailed& e)
-    {
-        throw FileError::Open();
-    }
-    catch (libguard::exception::GuardFileReadFailed& e)
-    {
-        throw FileError::Read();
-    }
-    catch (libguard::exception::GuardFileWriteFailed& e)
-    {
-        throw FileError::Write();
-    }
-    catch (libguard::exception::InvalidEntityPath& e)
-    {
-        throw type::CommonError::InvalidArgument();
-    }
-    catch (libguard::exception::AlreadyGuarded& e)
-    {
-        throw HardwareIsolationError::IsolatedAlready();
-    }
-    catch (libguard::exception::GuardFileOverFlowed& e)
-    {
-        throw type::CommonError::NotAllowed();
-    }
+
+    CALL_LIBGUARD_INTERFACE(records = libguard::getAll();)
 
     return records;
 }
 
 const fs::path getGuardFilePath()
 {
-
     fs::path guardfilePath;
 
-    try
-    {
-        guardfilePath = libguard::getGuardFilePath();
-    }
-    catch (libguard::exception::GuardFileOpenFailed& e)
-    {
-        throw FileError::Open();
-    }
-    catch (libguard::exception::GuardFileReadFailed& e)
-    {
-        throw FileError::Read();
-    }
-    catch (libguard::exception::GuardFileWriteFailed& e)
-    {
-        throw FileError::Write();
-    }
-    catch (libguard::exception::InvalidEntityPath& e)
-    {
-        throw type::CommonError::InvalidArgument();
-    }
-    catch (libguard::exception::AlreadyGuarded& e)
-    {
-        throw HardwareIsolationError::IsolatedAlready();
-    }
-    catch (libguard::exception::GuardFileOverFlowed& e)
-    {
-        throw type::CommonError::NotAllowed();
-    }
+    CALL_LIBGUARD_INTERFACE(guardfilePath = libguard::getGuardFilePath();)
 
     return guardfilePath;
 }
