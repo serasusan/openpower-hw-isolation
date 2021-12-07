54d60a2482c9e3afa60208d433918fde7bff24e1
Ramesh Iyyar <rameshi1@in.ibm.com>
2021-12-07 05:01:56 -0600

    common: Implemented the FFDCFiles class
    - In this patch implemented the FFDCFiles class to create
      the FFDCFile based on needs while trying to create
      the error log.
    
    - In this class added an API to collect the systemd journal traces
      based on the traces of the required application and created the FFDC
      file as "Text" format for the collected traces.
    
      - This class constructor provides a flag to the caller so that
        they can decide whether wants to collect traces or not based on
        their needs.
    
      - By default collecting the 10 traces for each requested application.
    
    - In this class added an API to create an FFDC file for the given
      callout details.
    
      - Note, The FFDC file for the callout will be created if the
        caller is filled with the details.
    
    - Also, it contains API to get FFDCFiles information (the type which
      is expected by the error log create interface) that can be
      passed with the error log create request.
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>
    Change-Id: I2d0cac61c19a8c9653c42c7585a64e35fd020ab8

--- include/common/error_log.hpp
+++ include/common/error_log.hpp
@@ -2,6 +2,9 @@
 
 #pragma once
 
+#include <systemd/sd-journal.h>
+
+#include <nlohmann/json.hpp>
 #include <xyz/openbmc_project/Logging/Create/server.hpp>
 #include <xyz/openbmc_project/Logging/Entry/server.hpp>
 
@@ -10,12 +13,18 @@ namespace hw_isolation
 namespace error_log
 {
 
+using json = nlohmann::json;
+
 using CreateIface = sdbusplus::xyz::openbmc_project::Logging::server::Create;
 
 using FFDCFormat = CreateIface::FFDCFormat;
 using FFDCSubType = uint8_t;
 using FFDCVersion = uint8_t;
 using FFDCFileFD = sdbusplus::message::unix_fd;
+
+using FFDCFileInfo =
+    std::tuple<FFDCFormat, FFDCSubType, FFDCVersion, FFDCFileFD>;
+using FFDCFilesInfo = std::vector<FFDCFileInfo>;
 
 /**
  * @class FFDCFile
@@ -143,6 +152,97 @@ class FFDCFile
     void removeFFDCFile();
 
 }; // end of FFDCFile class
+
+/**
+ * @class FFDCFiles
+ *
+ * @brief This class is used to manage the created ffdc files
+ */
+class FFDCFiles
+{
+  public:
+    FFDCFiles() = delete;
+    FFDCFiles(const FFDCFiles&) = delete;
+    FFDCFiles& operator=(const FFDCFiles&) = delete;
+    FFDCFiles(FFDCFiles&&) = delete;
+    FFDCFiles& operator=(FFDCFiles&&) = delete;
+
+    /**
+     * @brief Used to create the FFDC files based on the given inputs
+     *
+     * @param[in] collectTraces - Used to create the FFDC file for
+     *                            the traces if required.
+     * @param[in] calloutsDetails - Used to create the FFDC file for
+     *                              the callouts if exist.
+     */
+    explicit FFDCFiles(const bool collectTraces, const json& calloutsDetails);
+
+    /**
+     * @brief Used to get the created FFDC files details to add in the error
+     *        log
+     *
+     * @param[out] ffdcFilesInfo - Used to return FFDCFiles with its
+     *                             information
+     *
+     * @return void
+     */
+    void transformFFDCFiles(FFDCFilesInfo& ffdcFilesInfo);
+
+  private:
+    /**
+     * @brief Used to store the list of created FFDCFile
+     */
+    std::vector<std::unique_ptr<FFDCFile>> _ffdcFiles;
+
+    /**
+     * @brief Helper API to get the given field value from
+     *        the systemd journal context
+     *
+     * @param[in] journal - The systemd journal to get the field value
+     * @param[in] fieldName - The field name to get its value
+     *
+     * @return The field value on success
+     *         Empty optional on failures
+     */
+    std::optional<std::string>
+        sdjGetTraceFieldValue(sd_journal* journal,
+                              const std::string& fieldName);
+
+    /**
+     * @brief Helper API to get the last requested number of traces
+     *        from systemd journal entries based on the given field name
+     *        and value. By default, it will collect last 10 traces.
+     *
+     * @param[in] fieldName - The field name to search.
+     * @param[in] fieldValue - The field value to search.
+     * @param[in] maxReqTraces - The maximum number of traces to fetch.
+     *
+     * @return The list of traces from the systemd journal on success
+     *         Emptry optional on failure
+     */
+    std::optional<std::vector<std::string>>
+        sdjGetTraces(const std::string& fieldName,
+                     const std::string& fieldValue,
+                     const unsigned int maxReqTraces = 10);
+
+    /**
+     * @brief Used to collect the systemd journal traces and create the
+     *        FFDCFile
+     *
+     * @return Throw Exceptions on failure.
+     */
+    void createFFDCFileForTraces();
+
+    /**
+     * @brief Used to create the FFDC file with the given callout details
+     *
+     * @param[in] calloutsDetails - callouts detailsto create ffdc file
+     *
+     * @return Throw Exceptions on failure.
+     */
+    void createFFDCFileforCallouts(const json& calloutsDetails);
+
+}; // end of FFDCFiles class
 
 } // namespace error_log
 } // namespace hw_isolation
--- src/common/error_log.cpp
+++ src/common/error_log.cpp
@@ -7,6 +7,9 @@
 
 #include <phosphor-logging/elog.hpp>
 
+#include <iomanip>
+#include <sstream>
+
 namespace hw_isolation
 {
 namespace error_log
@@ -109,6 +112,262 @@ void FFDCFile::removeFFDCFile()
 {
     close(_fd);
     std::remove(_fileName.c_str());
+}
+
+FFDCFiles::FFDCFiles(const bool collectTraces, const json& calloutsDetails)
+{
+    // Create FFDCFile for the traces if requested to collect
+    if (collectTraces)
+    {
+        try
+        {
+            createFFDCFileForTraces();
+        }
+        catch (const std::exception& e)
+        {
+            /**
+             * Don't throw the exception, we should create FFDCFiles as much as
+             * possible to create the error log.
+             */
+            log<level::ERR>(
+                fmt::format("Exception [{}], failed to collect traces",
+                            e.what())
+                    .c_str());
+        }
+    }
+
+    // Create FFDCFile for the JSON data if it is filled
+    if (!calloutsDetails.is_null())
+    {
+        try
+        {
+            createFFDCFileforCallouts(calloutsDetails);
+        }
+        catch (const std::exception& e)
+        {
+            /**
+             * Don't throw the exception, we should create FFDCFiles as much as
+             * possible to create the error log.
+             */
+            log<level::ERR>(
+                fmt::format("Exception [{}], failed to include callout details",
+                            e.what())
+                    .c_str());
+        }
+    }
+}
+
+std::optional<std::string>
+    FFDCFiles::sdjGetTraceFieldValue(sd_journal* journal,
+                                     const std::string& fieldName)
+{
+    const char* data{nullptr};
+    size_t length{0};
+
+    if (auto rc =
+            sd_journal_get_data(journal, fieldName.c_str(),
+                                reinterpret_cast<const void**>(&data), &length);
+        rc == 0)
+    {
+        // Get field value, constructing by the returned length so we can use
+        // std::string::c_str if needs null-terminated string.
+        std::string fieldValue(data, length);
+
+        // The data returned  by sd_journal_get_data will be prefixed with the
+        // field name and "=".
+        if (auto fieldValPos = fieldValue.find('=');
+            fieldValPos != std::string::npos)
+        {
+            // Just return field value alone instead of with field name
+            return fieldValue.substr(fieldValPos + 1);
+        }
+        else
+        {
+            // Should not happen as per sd_journal_get_data documentation
+            log<level::ERR>(
+                fmt::format("Failed to find the journal field "
+                            "separator [=] in the retrieved field [{}]",
+                            fieldValue)
+                    .c_str());
+        }
+    }
+    else
+    {
+        log<level::ERR>(
+            fmt::format("Failed to get the given journal "
+                        "field [{}] value errorno [{}] and errormsg [{}]",
+                        fieldName, rc, strerror(rc))
+                .c_str());
+    }
+    return std::nullopt;
+}
+
+std::optional<std::vector<std::string>>
+    FFDCFiles::sdjGetTraces(const std::string& fieldName,
+                            const std::string& fieldValue,
+                            const unsigned int maxReqTraces)
+{
+    // First get the systemd journal context
+    sd_journal* journal;
+    if (auto rc = sd_journal_open(&journal, SD_JOURNAL_LOCAL_ONLY); rc == 0)
+    {
+        std::vector<std::string> traces;
+
+        // Look the systemd journal entries from the reverse order
+        // so that we will get requested latest number of traces
+        SD_JOURNAL_FOREACH_BACKWARDS(journal)
+        {
+            auto retValue = sdjGetTraceFieldValue(journal, fieldName);
+
+            if (!retValue.has_value() || *retValue != fieldValue)
+            {
+                // The retrieved journal entry is not expected one
+                continue;
+            }
+
+            // Get SYSLOG_IDENTIFIER field (process that logged trace)
+            std::string sysLogId;
+            if (fieldName == std::string("SYSLOG_IDENTIFIER"))
+            {
+                sysLogId = fieldValue;
+            }
+            else
+            {
+                if (retValue =
+                        sdjGetTraceFieldValue(journal, "SYSLOG_IDENTIFIER");
+                    retValue.has_value())
+                {
+                    sysLogId = *retValue;
+                }
+            }
+
+            // Get _PID field
+            std::string pid;
+            if (retValue = sdjGetTraceFieldValue(journal, "_PID");
+                retValue.has_value())
+            {
+                pid = *retValue;
+            }
+
+            // Get MESSAGE field
+            std::string message;
+            if (retValue = sdjGetTraceFieldValue(journal, "MESSAGE");
+                retValue.has_value())
+            {
+                message = *retValue;
+            }
+
+            // Get timestamp
+            std::string timeStamp;
+            uint64_t usec{0};
+            if (0 == sd_journal_get_realtime_usec(journal, &usec))
+            {
+                // Convert realtime microseconds to date format
+                // E.g.: Dec 07 2021 15:48:29
+                std::time_t timeInSecs = usec / 1000000;
+                std::stringstream ss;
+                ss << std::put_time(std::localtime(&timeInSecs),
+                                    "%b %d %Y %H:%M:%S");
+
+                timeStamp = ss.str();
+            }
+
+            // Format: Timestamp : ProcessName[ProcessPID] : Message
+            std::stringstream trace;
+            trace << timeStamp << " : " << sysLogId << "[" << pid
+                  << "] : " << message;
+
+            /**
+             * Add the retrieved trace before first trace since we usually
+             * look at the traces from bottom to up to get to know
+             * whats happend from the current trace to previous traces.
+             *
+             * The systemd journal entries are fetched in the reverse order.
+             */
+            traces.emplace(traces.begin(), trace.str());
+
+            // Don't go for the next systemd journal entry if the maximum
+            // required traces are fetched
+            if (traces.size() == maxReqTraces)
+            {
+                break;
+            }
+        }
+
+        // Close journal when fetched the systemd journal traces
+        sd_journal_close(journal);
+
+        if (!traces.empty())
+        {
+            return traces;
+        }
+        else
+        {
+            log<level::INFO>(
+                fmt::format("Dont have any systemd journal traces"
+                            "for the given field name [{}] and value [{}].",
+                            fieldName, fieldValue)
+                    .c_str());
+        }
+    }
+    else
+    {
+        log<level::ERR>(
+            fmt::format("Failed to get the systemd journal "
+                        "traces for the given field name [{}] and value [{}]. "
+                        "ErrorNo [{}] and ErrorMsg [{}]",
+                        fieldName, fieldValue, rc, strerror(rc))
+                .c_str());
+    }
+
+    return std::nullopt;
+}
+
+void FFDCFiles::createFFDCFileForTraces()
+{
+    // Add required applications to get their traces
+    static const std::vector<std::string> apps{"openpower-hw-isolation"};
+
+    for (const auto& app : apps)
+    {
+        // By default we can get 10 traces
+        auto traces = sdjGetTraces("SYSLOG_IDENTIFIER", app);
+
+        if (traces.has_value() && !traces->empty())
+        {
+            std::string data;
+            for (const auto& trace : *traces)
+            {
+                data.append(trace);
+                if (!trace.ends_with('\n'))
+                {
+                    data.append("\n");
+                }
+            }
+
+            // FFDC Subtype and Version is "0" for the FFDCFormat::Text
+            _ffdcFiles.emplace_back(
+                std::make_unique<FFDCFile>(FFDCFormat::Text, 0, 0, data));
+        }
+    }
+}
+
+void FFDCFiles::createFFDCFileforCallouts(const json& calloutsDetails)
+{
+    // FFDC Subtype and Version should be "0xCA" and "0x01" respectively
+    // for the callouts
+    _ffdcFiles.emplace_back(std::make_unique<FFDCFile>(
+        FFDCFormat::JSON, 0xCA, 0x01, calloutsDetails.dump()));
+}
+
+void FFDCFiles::transformFFDCFiles(FFDCFilesInfo& ffdcFilesInfo)
+{
+    std::transform(_ffdcFiles.begin(), _ffdcFiles.end(),
+                   std::back_inserter(ffdcFilesInfo), [](const auto& ffdcFile) {
+                       return std::make_tuple(
+                           ffdcFile->getFormat(), ffdcFile->getSubType(),
+                           ffdcFile->getVersion(), ffdcFile->getFD());
+                   });
 }
 
 } // namespace error_log
