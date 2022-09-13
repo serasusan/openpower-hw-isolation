d86ab52198e301e266fb310e2dcbf2a5d621ca17
Marri Devender Rao <devenrao@in.ibm.com>
2022-09-13 02:00:58 -0500

    record: fix guard record not created during checkstop
    When an error is injected onto a core at runtime system checkstops
    guards the core and creates an hw-isolation entry
    
    hw-isolation as part of creating isolation entry sets the enabled
    property of the core D-Bus object hosted by PLDM.
    
    In a rare window, PLDM hangs during checkstop and recovers back. If
    any request comes to enable the D-Bus property of the core D-Bus
    object hosted by PLDM in this window the same will not be honored.
    
    As D-Bus setProperty method fails current implementation creates the
    isolation entry but not the guard record.
    
    Now modified to log an error and continue to guard the isolated
    hardware instead of the throwing exception.
    
    The enable property will remain false for now until the isolation
    service restarts and records are refreshed.
    
    The customer would fail to see a guard record due to checkstop and
    in absence of a guard record, the subsequent boots would try to boot
    the bad core (which should have guarded in the first place)
    
    In the future when core D-Bus objects will be hosted by EntityManager
    by then we will not have any issues.
    
    It is difficult to reproduce the issue, so tested by micking throwing
    exception before D-Bus call to PLDM to enable the property.
    
    ---Tested bad bath--
    Sep 13 16:47:40 p10bmc openpower-hw-isolation[5623]: mimicking pldm stuck
    obj/xyz/openbmc_project/inventory/system/chassis/motherboard/dcm0/cpu0/core13
    resolved false
    Sep 13 16:47:40 p10bmc openpower-hw-isolation[5623]: Exception [mick pldm
    stuck], failed to enable guarded entry
    Sep 13 16:51:38 p10bmc openpower-hw-isolation[5623]: update record obj
    [/xyz/openbmc_project/inventory/system/chassis/motherboard/dcm0/cpu0/core13]
    resolved false
    
    Verified that the record is listed in "Deconfiguration records" page in GUI.
    Verified that the core is marked as "Deconfigure" in Hadware deconfiguration
    page
    
    ---Tested good path---
    root@p10bmc:/tmp# guard -l
    ID         | ERROR      | Type            | Path
    0x00000001 | 0x50001890 | fatal           |
    physical:sys-0/node-0/proc-0/eq-6/fc-1/core-1
    
    Signed-off-by: Marri  Rao <devenrao@in.ibm.com>
    Change-Id: I0eb710f4213a9c58136da5bfa0d5966744d0af21

--- src/hw_isolation_record/manager.cpp
+++ src/hw_isolation_record/manager.cpp
@@ -648,6 +648,8 @@ void Manager::updateEntryForRecord(const openpower_guard::GuardRecord& record,
         updated = true;
     }
 
+    utils::setEnabledProperty(_bus, *isolatedHwInventoryPath, false);
+
     if (updated)
     {
         // Existing entry might be overwritten if that's meets certain
